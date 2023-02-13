c18f0287503e54bb0de5dcde6720b520fc7125c0
Collin Rukundo <collin@matchstick.ug>
2023-02-13 22:56:41 +0300

    init etta-ui-docs

---
+++ docs/intro.md
@@ -0,0 +1,36 @@
+---
+title: Introduction
+path: /intro/
+sidebar_position: 1
+---
+
+## What?
+
+EttaUI is an open-source, opinionated React Native component library for building quality, beautiful, accessible bitcoin apps. The components here are inspired by and built on the work of the **[Bitcoin Design Community](https://bitcoin.design)** whose [guide](https://bitcoin.design/guide/) informs and influences the user experience across multiple bitcoin projects. They are awesome!
+
+## Why?
+
+I am currently building **[EttaWallet](https://github.com/EttaWallet)**, a concept bitcoin/lightning wallet. I needed a set of beautifully designed and accesible native components with unified styling across Android and IOS platforms. While this was built to benefit me, I believe it will help others exponentially. Specifically:
+
+- Teams will spend more time solving complex problems, and less time on menial tasks around recreating multiple atoms and molecules.
+- The components are built to take advantage of [accessibility APIs](https://reactnative.dev/docs/accessibility) that work alongisde assistive technologies to ensure an inclusive experience for users on Android and IOS.
+
+## For whom?
+
+It's open source so anybody can use this but ideally, most of the components here-in will be useful for anyone building a bitcoin project with React Native. It would be nice to know if this helps you so send me a **[DM](https://twitter.com/rukundo__)** but really, use it. No attribution necessary.
+
+You can use this project by plugging it into your project as a dependency or, you can simply copy and paste those components into your existing projects.
+
+## When update?
+
+:::caution
+
+This is a work-in-progress even though I am currently using this in production for EttaWallet.
+
+:::
+
+This project will continue to be updated alongisde the Bitcoin Design Guide, and as and when I need more components for EttaWallet.
+
+## Contributions welcome?
+
+Of course. You can open an issue or submit a pull request on **[GitHub](https://github.com/EttaWallet/etta-ui)**.
---
+++ docusaurus.config.js
@@ -0,0 +1,148 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'EttaWallet UI Library',
+  tagline:
+    'Open-source, opinionated React Native component library for building quality, beautiful, accessible bitcoin apps',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://ettawallet.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+  clientModules: [require.resolve('./plugins/snackPlayerInitializer.js')],
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'EttaWallet', // Usually your GitHub org/user name.
+  projectName: 'etta-ui', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  scripts: [{ src: '//snack.expo.io/embed.js', defer: true }],
+  // plugins: ['./plugins/etta-ui-web.js'],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+
+          remarkPlugins: [require('./plugins/remark-snackplayer')],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'EttaUI',
+        logo: {
+          alt: 'EttaWallet Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Components',
+          },
+          {
+            href: 'https://github.com/EttaWallet',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Components',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} EttaWallet, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+    }),
+};
+
+module.exports = config;
